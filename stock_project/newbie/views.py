0d3ebed9daf121efba992114b0864f90587dbca6
KAI-SIANG YOU <s10814092@gm.cyut.edu.tw>
2022-10-14 18:39:25 +0800

    Add files via upload

--- stock_project/newbie/views.py
+++ stock_project/newbie/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.template.context_processors import request
-from django.http.response import HttpResponse
 
 def newbie(request):
     return render(request,'newbie/newbie.html')
