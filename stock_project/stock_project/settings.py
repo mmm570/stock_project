63d0886ed6304258ddd03e4a53b2a29131e8ab5e
KAI-SIANG YOU <s10814092@gm.cyut.edu.tw>
2022-10-14 14:31:43 +0800

    Update settings.py

--- stock_project/stock_project/settings.py
+++ stock_project/stock_project/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'stock_project.urls'
@@ -136,9 +136,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = BASE_DIR /'staticfiles'
+STATIC_ROOT = /'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
